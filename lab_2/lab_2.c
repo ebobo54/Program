396115d61dd2ee7635c3e006a0014b02d9891506
Eb0b0l0 <daniil.gilew@mail.ru>
2023-10-03 18:51:46 +0500

    1

--- lab_2/lab_2.c
+++ lab_2/lab_2.c
@@ -1,28 +1,24 @@
 #include <stdio.h>
 #include <math.h>
-#include <stdlib.h>
-
-double f;
-double x;
-
 
 int main()
 {
 
-    double x = 0.0;
-    double y;
-
-    double s;
-    printf("Enter s -> ");
+    double y,s,x = 0.0;
+    printf("Enter number-> ");
     scanf("%lf", &s);
+    int n;
+    n=2/s+1;
 
-    while (0<=x && x<=1){
-        y = cos(x+ pow(x, 3));
-        printf("%f %f\n", x, y);
-    }
-    while(1 < x && x <= 2){
-        y = exp(pow(-x,2))-pow(x,2)+2*x;
-        printf("%f %f\n", x, y);
+    while(n)
+    {
+        if (x >= 0 && x <= 1)
+            y = cos(x + pow(x, 3.0));
+        if (x > 1 && x <= 2)
+            y = exp(-pow(x, 2.0) - pow(x, 2.0) + 2.0 * x);
+        printf("%lf %lf\n", x, y);
+        x += s;
+        n = n - 1;
     }
     return 0;
 }
