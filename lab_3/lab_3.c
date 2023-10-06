bc49a1f0dd2fa7c2b2dbcbcb2262ad79f08b27f3
Eb0b0l0 <daniil.gilew@mail.ru>
2023-10-06 14:33:56 +0500

    1

--- lab_3/lab_3.c
+++ lab_3/lab_3.c
@@ -1,2 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+void fill(int n, int a[])
+{
+    int i;
+    for (i = 0; i < n; i++)
+        a[i] = rand () % 101 - 50;
+}
 
+int main()
+{
+    srand(time(NULL));
+    int n;
+    printf("n -> ");
+    scanf("%d", &n);
+    int A[n];
+    fill(n, A);
+    int i;
+    for (i = 0; i < n; i++)
+        printf("%4d", A[i]);
+    printf("\n");
+    return 0;
+}
