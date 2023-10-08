8b5adc94c48cb6d97eac8eacc595cb4fbe3f4756
Eb0b0l0 <daniil.gilew@mail.ru>
2023-10-08 14:42:13 +0500

    1

--- lab_3/lab_3.c
+++ lab_3/lab_3.c
@@ -26,7 +26,7 @@ int findMaxIndex(int n, int a[]) {
     return maxIndex;
 }
 
-void processArray(int n, int a[]) {
+void process(int n, int a[]) {
     int minIndex = findMinIndex(n, a);
     int maxIndex = findMaxIndex(n, a);
 
@@ -59,7 +59,7 @@ int main() {
         printf("%4d", A[i]);
     printf("\n");
 
-    processArray(n, A);
+    process(n, A);
 
     printf("Ne original\n");
     for (int i = 0; i < n; i++)
