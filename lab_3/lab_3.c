17c98ebe15275cc2e16b1428d9cf9da059aa46d8
Eb0b0l0 <daniil.gilew@mail.ru>
2023-10-08 14:32:07 +0500

    1

--- lab_3/lab_3.c
+++ lab_3/lab_3.c
@@ -2,24 +2,69 @@
 #include <stdlib.h>
 #include <time.h>
 
-void fill(int n, int a[])
-{
+void fill(int n, int a[]) {
     int i;
     for (i = 0; i < n; i++)
-        a[i] = rand () % 101 - 50;
+        a[i] = rand() % 101 - 50;
 }
 
-int main()
-{
+int findMinIndex(int n, int a[]) {
+    int minIndex = 0;
+    for (int i = 1; i < n; i++) {
+        if (a[i] < a[minIndex])
+            minIndex = i;
+    }
+    return minIndex;
+}
+
+int findMaxIndex(int n, int a[]) {
+    int maxIndex = 0;
+    for (int i = 1; i < n; i++) {
+        if (a[i] > a[maxIndex])
+            maxIndex = i;
+    }
+    return maxIndex;
+}
+
+void processArray(int n, int a[]) {
+    int minIndex = findMinIndex(n, a);
+    int maxIndex = findMaxIndex(n, a);
+
+    int sumIndices = minIndex + maxIndex;
+
+    if (abs(sumIndices) < abs(a[minIndex])) {
+        a[minIndex] = sumIndices;
+    } else if (abs(sumIndices) > abs(a[maxIndex])) {
+        a[maxIndex] = sumIndices;
+    } else {
+        int start = (minIndex < maxIndex) ? minIndex : maxIndex;
+        int end = (minIndex > maxIndex) ? minIndex : maxIndex;
+
+        for (int i = start + 1; i < end; i++) {
+            a[i] = 0;
+        }
+    }
+}
+
+int main() {
     srand(time(NULL));
     int n;
     printf("n -> ");
     scanf("%d", &n);
     int A[n];
     fill(n, A);
-    int i;
-    for (i = 0; i < n; i++)
+
+    printf("Original\n");
+    for (int i = 0; i < n; i++)
         printf("%4d", A[i]);
     printf("\n");
+
+    processArray(n, A);
+
+    printf("Ne original\n");
+    for (int i = 0; i < n; i++)
+        printf("%4d", A[i]);
+    printf("\n");
+
     return 0;
 }
