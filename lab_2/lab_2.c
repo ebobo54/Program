63657dc88a56fb5bbdc57b2c808d1050ff6afec0
Quliet <parenko2000@mail.ru>
2023-10-23 15:39:04 +0500

    1

--- lab_2/lab_2.c
+++ lab_2/lab_2.c
@@ -5,7 +5,7 @@
 int main()
 {
 
-    double y,s,x = 0.0;
+    double y,s,x = 0;
     printf("Enter number-> ");
     scanf("%lf", &s);
     int n;
@@ -14,9 +14,9 @@ int main()
     while(n)
     {
         if (x >= 0 && x <= 1)
-            y = cos(x + pow(x, 3.0));
+            y = cos(x + pow(x, 3));
         if (x > 1 && x <= 2)
-            y = exp(-pow(x, 2.0) - pow(x, 2.0) + 2.0 * x);
+            y = exp(-pow(x, 2) - pow(x, 2) + 2 * x);
         printf("%lf %lf\n", x, y);
         x += s;
         n = n - 1;
