921ba0d781b8b59dc1c064e44484e3488a0757e7
Quliet <parenko2000@mail.ru>
2023-10-30 15:08:53 +0500

    1

--- lab_5/lab_5.py
+++ lab_5/lab_5.py
@@ -1,1 +1,18 @@
 import matplotlib.pyplot as plt
+x = np.linspace(0, 10, 50)
+y1 = x
+# Квадратичная зависимость
+y2 = [i**2 for i in x]
+# Построение графиков
+plt.figure(figsize=(9, 9))
+plt.subplot(2, 1, 1)
+plt.plot(x, y1) # построение графика
+plt.title('Зависимости: y1 = x, y2 = x^2') # заголовок
+plt.ylabel('y1', fontsize=14) # ось ординат
+plt.grid(True) # включение отображение сетки
+plt.subplot(2, 1, 2)
+plt.plot(x, y2) # построение графика
+plt.xlabel('x', fontsize=14) # ось абсцисс
+plt.ylabel('y2', fontsize=14) # ось ординат
+plt.grid(True) # включение отображение сетки
+plt.show()
