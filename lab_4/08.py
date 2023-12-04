f856503860dc1de1f0ac1cc1b52a2a643d1bb9c8
Quliet <parenko2000@mail.ru>
2023-12-04 15:56:19 +0500

    1

--- lab_4/08.py
+++ lab_4/08.py
@@ -15,17 +15,13 @@ garden_set = set(garden)
 meadow_set = set(meadow)
 # выведите на консоль все виды цветов
 # TODO здесь ваш код
-print("Все виды цветов в саду и на лугу:")
 print(garden_set.union(meadow_set))
 # выведите на консоль те, которые растут и там и там
 # TODO здесь ваш код
-print("Цветы, которые растут и в саду и на лугу:")
-print(garden_set.intersection(meadow_set))
+print(garden_set & meadow_set)
 # выведите на консоль те, которые растут в саду, но не растут на лугу
 # TODO здесь ваш код
-print("Цветы, которые растут в саду, но не растут на лугу:")
 print(garden_set.difference(meadow_set))
 # выведите на консоль те, которые растут на лугу, но не растут в саду
 # TODO здесь ваш код
-print("Цветы, которые растут на лугу, но не растут в саду:")
 print(meadow_set.difference(garden_set))
