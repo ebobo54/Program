f2e2f7aaf97a1eabf8c6fda487d333050437737d
Eb0b0l0 <89797412+ebobo54@users.noreply.github.com>
2023-12-23 13:51:17 +0500

    1

--- lab_7/lab_7.py
+++ lab_7/lab_7.py
@@ -18,6 +18,21 @@
 # print(intersect_non_recursive([1, 2, 3, 4], [2, 3, 4, 6, 8])) 
 # print(intersect_non_recursive([5, 8, 2], [2, 9, 1]))            
 # print(intersect_non_recursive([5, 8, 2], [7, 4]))     
+
+# def intersect_with_duplicates(list1, list2):
+#     result = []
+#     for item in list1:
+#         if item in list2:
+#             result.append(item)
+#             list2.remove(item)
+#     return result
+
+# print(intersect_with_duplicates([1, 2,2, 3, 4], [2, 3, 4, 2,6, 8])) 
+# print(intersect_with_duplicates([5, 8, 2], [2, 9, 1]))            
+# print(intersect_with_duplicates([5, 8, 2], [7, 4]))  
+
+from collections import Counter
+
 def intersect_with_duplicates(list1, list2):
     result = []
     for item in list1:
@@ -26,6 +41,25 @@ def intersect_with_duplicates(list1, list2):
             list2.remove(item)
     return result
 
-print(intersect_with_duplicates([1, 2,2, 3, 4], [2, 3, 4, 2,6, 8])) 
-print(intersect_with_duplicates([5, 8, 2], [2, 9, 1]))            
-print(intersect_with_duplicates([5, 8, 2], [7, 4]))  
+def intersect_non_recursive(list1, list2):
+    result = []
+    
+    counter_list1 = Counter(list1)
+    counter_list2 = Counter(list2)
+    
+    common_elements = counter_list1 & counter_list2
+
+    for element, count in common_elements.items():
+        result.extend([element] * count)
+
+    return result
+
+# Результаты для intersect_recursive
+print(intersect_with_duplicates([1,2, 2, 3, 4], [2, 3, 4, 6, 8,2]))  
+print(intersect_with_duplicates([5, 8, 2], [2, 9, 1]))          
+print(intersect_with_duplicates([5, 8, 2], [7, 4]))              
+
+# Результаты для intersect_non_recursive
+print(intersect_non_recursive([2,1, 2, 3, 4], [2, 3, 4, 6, 8,2])) 
+print(intersect_non_recursive([5, 8, 2], [2, 9, 1]))            
+print(intersect_non_recursive([5, 8, 2], [7, 4]))
