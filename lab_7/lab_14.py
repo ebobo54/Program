8d5f1908d42d51d80d2843e49faa51000b039ebc
Eb0b0l0 <89797412+ebobo54@users.noreply.github.com>
2023-12-26 12:50:20 +0500

    1

--- lab_7/lab_14.py
+++ lab_7/lab_14.py
@@ -1,11 +1,26 @@
+# def nested_sqrt_recursive(n):
+#     if n == 1:
+#         return 3
+#     else:
+#         return (3 + nested_sqrt_recursive(n - 1)) ** 0.5
+
+# def nested_sqrt_non_recursive(n):
+#     result = 3
+#     for _ in range(n - 1):
+#         result = (3 + result) ** 0.5
+#     return result
+
+# print(nested_sqrt_recursive(1))
+# print(nested_sqrt_non_recursive(1))
+
 def nested_sqrt_recursive(n):
     if n == 1:
-        return 3
+        return 0
     else:
         return (3 + nested_sqrt_recursive(n - 1)) ** 0.5
 
 def nested_sqrt_non_recursive(n):
-    result = 3
+    result = 0
     for _ in range(n - 1):
         result = (3 + result) ** 0.5
     return result
--- lab_8/lab_24.py
+++ lab_8/lab_24.py
@@ -35,6 +35,85 @@
 #     print(fibonacci_gen())
 
 
+# def cache_decorator(func):
+#     cache = {}
+
+#     def wrapper(*args, **kwargs):
+#         key = (args, frozenset(kwargs.items()))
+
+#         if key in cache:
+#             print(f"Cache hit for {func.__name__}{args}{kwargs}")
+#             return cache[key]
+
+#         result = func(*args, **kwargs)
+
+#         cache[key] = result
+
+#         print(f"Cache miss for {func.__name__}{args}{kwargs}")
+
+#         return result
+
+#     return wrapper
+
+# @cache_decorator
+# def fibonacci_closure():
+#     a, b = 0, 1
+
+#     def generate_fibonacci():
+#         nonlocal a, b
+#         result = a
+#         a, b = b, a + b
+#         return result
+
+#     return generate_fibonacci
+
+# fibonacci_gen = fibonacci_closure()
+
+# for _ in range(10):
+#     print(fibonacci_gen())
+
+
+# def cache_decorator(func):
+#     cache = {}
+
+#     def wrapper(*args, **kwargs):
+#         if func.__name__ == 'fibonacci_closure':
+#             key = 'fibonacci_closure'
+#         else:
+#             key = (args, frozenset(kwargs.items()))
+
+#         if key in cache:
+#             print(f"Cache hit for {func.__name__}{args}{kwargs}")
+#             return cache[key]
+
+#         result = func(*args, **kwargs)
+
+#         if key != 'fibonacci_closure':
+#             cache[key] = result
+
+#         print(f"Cache miss for {func.__name__}{args}{kwargs}")
+
+#         return result
+
+#     return wrapper
+
+# @cache_decorator
+# def fibonacci_closure():
+#     a, b = 0, 1
+
+#     def generate_fibonacci():
+#         nonlocal a, b
+#         result = a
+#         a, b = b, a + b
+#         return result
+
+#     return generate_fibonacci
+
+# fibonacci_gen = fibonacci_closure()
+
+# for _ in range(10):
+#     print(fibonacci_gen())
+
 def cache_decorator(func):
     cache = {}
 
@@ -55,19 +134,32 @@ def cache_decorator(func):
 
     return wrapper
 
-@cache_decorator
 def fibonacci_closure():
     a, b = 0, 1
 
     def generate_fibonacci():
         nonlocal a, b
-        result = a
         a, b = b, a + b
-        return result
+        return a
 
     return generate_fibonacci
 
-fibonacci_gen = fibonacci_closure()
+fibonacci_gen = cache_decorator(fibonacci_closure())  # Применяем декоратор к generate_fibonacci
 
+def add(a, b):
+    print("Executing add function")
+    return a + b + fibonacci_gen()
+
+def multiply(x, y):
+    print("Executing multiply function")
+    return x * y + fibonacci_gen()
+
+# Вызываем функции
+print(add(2, 3))
+print(add(2, 3))
+print(multiply(4, 5))
+print(multiply(4, 5))
+
+fibonacci_gen = fibonacci_closure()
 for _ in range(10):
     print(fibonacci_gen())
